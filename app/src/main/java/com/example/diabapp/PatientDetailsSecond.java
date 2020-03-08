7d32fd056004b9085ec2db7d853dec6af161f191
Milinda1313 <smilinda.sandaruwan@gmail.com>
2020-03-08 20:00:54 +0530

    added navigation paths

--- app/src/main/java/com/example/diabapp/PatientDetailsSecond.java
+++ app/src/main/java/com/example/diabapp/PatientDetailsSecond.java
@@ -2,18 +2,21 @@ package com.example.diabapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 import com.example.diabapp.constants.Constatnts;
 
 public class PatientDetailsSecond extends AppCompatActivity implements
-        AdapterView.OnItemSelectedListener{
+        AdapterView.OnItemSelectedListener {
 
-    Spinner jMaritalSpinner,jFoodPatternSpinner;
+    Spinner jMaritalSpinner, jFoodPatternSpinner;
+    Button jPatientDetailsNextBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +24,50 @@ public class PatientDetailsSecond extends AppCompatActivity implements
         setContentView(R.layout.activity_patient_details_second);
         casting();
 
+        nextBtnClick();
+
         jMaritalSpinner.setOnItemSelectedListener(this);
         jFoodPatternSpinner.setOnItemSelectedListener(this);
 
         //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter marital = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Constatnts.MARITAL);
+        ArrayAdapter marital = new ArrayAdapter(this, android.R.layout.simple_spinner_item, Constatnts.MARITAL);
         marital.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         jMaritalSpinner.setAdapter(marital);
 
         //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter food = new ArrayAdapter(this,android.R.layout.simple_spinner_item, Constatnts.FOOD_PATTERN);
+        ArrayAdapter food = new ArrayAdapter(this, android.R.layout.simple_spinner_item, Constatnts.FOOD_PATTERN);
         food.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //Setting the ArrayAdapter data on the Spinner
         jFoodPatternSpinner.setAdapter(food);
+
+
     }
 
 
-    public void casting()
+    public void casting() {
+        jPatientDetailsNextBtn = findViewById(R.id.aPatientDetailsNext);
+        jMaritalSpinner = findViewById(R.id.aMaritalSpinner);
+        jFoodPatternSpinner = findViewById(R.id.aFoodPatternSpinner);
+    }
+
+    public void nextBtnClick()
     {
-      jMaritalSpinner=findViewById(R.id.aMaritalSpinner);
-      jFoodPatternSpinner=findViewById(R.id.aFoodPatternSpinner);
+        jPatientDetailsNextBtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        moveToNextPage();
+                    }
+                }
+        );
     }
 
+    public void moveToNextPage()
+    {
+        Intent intent = new Intent(this, QuestionsScreen.class);
+        startActivity(intent);
+    }
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
